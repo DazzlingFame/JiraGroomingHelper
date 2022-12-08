07c1a5d5e54e0cecdcec371298b49caba5c3f041
DazzlingFame <narsel1995@mail.ru>
2022-12-08 20:20:36 +0300

    format updates

--- src/index.js
+++ src/index.js
@@ -25,53 +25,65 @@ function get_random(list) {
   return list[Math.floor(Math.random() * list.length)];
 }
 
-const notifyAboutNotStudy = async () => {
+const notifyAboutStudy = async (localMode = false) => {
   const sprints = (await jira.getAllSprints(40)).values;
   const next_sprint = sprints.find(
     (sprint) => sprint.state === "future" && !sprint.completeDate
   );
-  const tasks = await jira.searchJira(
-    `Sprint = "${next_sprint.name}" and "Story Points[Number]" is EMPTY and (status = Study or status = Analysis) and assignee is EMPTY`
-  );
-  const messages = tasks.issues.map((t) => {
-    const main_component = t.fields.components[0]
-      ? t.fields.components[0].name
-      : "Product";
-    const responsibles = RESPONSIBLE_MAP[main_component]
-      ? get_random(RESPONSIBLE_MAP[main_component])
-      : "- нет компонентов - <@karepovals>";
-    return `https://profiru.atlassian.net/browse/${t.key} - ${t.fields.summary} - нужно взять на проработку для сл. спринта (${next_sprint.name}) - ${responsibles}`;
-  });
-  const message = messages.join("\n\n");
-  console.log(message);
-  await webhook.send({
-    text: message,
-  });
-};
 
-const notifyAboutStudy = async () => {
-  const sprints = (await jira.getAllSprints(40)).values;
-  const next_sprint = sprints.find(
-    (sprint) => sprint.state === "future" && !sprint.completeDate
-  );
   const tasks = await jira.searchJira(
     `Sprint = "${next_sprint.name}" and "Story Points[Number]" is EMPTY and (status = Study or status = Analysis) and assignee is not EMPTY`
   );
-  const messages = tasks.issues.map((t) => {
-    const assignee = t.fields.assignee.emailAddress
-      ? t.fields.assignee.emailAddress.split("@")[0]
+
+  const messages = tasks.issues.map((task) => {
+    const assignee = task.fields.assignee.emailAddress
+      ? task.fields.assignee.emailAddress.split("@")[0]
       : "karepovals";
-    return `https://profiru.atlassian.net/browse/${t.key} - ${t.fields.summary}  - <@${assignee}>`;
+
+    return `<https://profiru.atlassian.net/browse/${task.key}|${task.fields.summary}>  - <@${assignee}>`;
   });
   const message =
-    "У нас на проработке следующие задачи, если проработка завершена, переведите в статус Awaiting Estimate:\n" +
-    messages.join("\n\n\n");
-  await webhook.send({
-    text: message,
-  });
+    "*У нас на проработке следующие задачи, если проработка завершена, переведите в статус Awaiting Estimate:*\n" +
+    messages.join("\n");
+  console.log(message);
+  if (!localMode) {
+    await webhook.send({
+      text: message,
+    });
+  }
 };
 
-const notifyAboutGrooming = async () => {
+const notifyAboutNotStudy = async (localMode = false) => {
+  const sprints = (await jira.getAllSprints(40)).values;
+  const next_sprint = sprints.find(
+      (sprint) => sprint.state === "future" && !sprint.completeDate
+  );
+  const tasks = await jira.searchJira(
+      `Sprint = "${next_sprint.name}" and "Story Points[Number]" is EMPTY and (status = Study or status = Analysis) and assignee is EMPTY`
+  );
+
+  const messages = tasks.issues.map((task) => {
+    const main_component = task.fields.components[0]
+        ? task.fields.components[0].name
+        : "Product";
+
+    const responsibles = RESPONSIBLE_MAP[main_component]
+        ? get_random(RESPONSIBLE_MAP[main_component])
+        : "- нет компонентов - <@karepovals>";
+    return `<https://profiru.atlassian.net/browse/${task.key}|${task.fields.summary}> - ${responsibles}`;
+  });
+  const message =
+      `\n\n*Нужно взять на проработку для сл. спринта (${next_sprint.name})*\n` +
+      messages.join("\n");
+  console.log(message);
+  if (!localMode) {
+    await webhook.send({
+      text: message,
+    });
+  }
+};
+
+const notifyAboutGrooming = async (localMode = false) => {
   const sprints = (await jira.getAllSprints(40)).values;
   const next_sprint = sprints.find(
     (sprint) => sprint.state === "future" && !sprint.completeDate
@@ -82,27 +94,26 @@ const notifyAboutGrooming = async () => {
   const messages = tasks.issues.map((t) => {
     return `https://profiru.atlassian.net/browse/${t.key} - ${t.fields.summary}`;
   });
-  const message =
-    "У нас проработаны следующие задачи для оценки сегодня:\n " +
-    messages.join("\n\n\n");
-  await webhook.send({
-    text: message,
-  });
+  const message = messages.length
+    ? "\n\n*У нас проработаны следующие задачи для оценки сегодня:*\n " +
+      messages.join("\n")
+    : "\n\n*Нет проработанных задач для оценки*";
+  console.log(message);
+  if (!localMode) {
+    await webhook.send({
+      text: message,
+    });
+  }
 };
 
-const t = async () => {
-  await webhook.send({
-    text: "====================================================================================",
-  });
-  await notifyAboutStudy();
-  await webhook.send({
-    text: "===================================================================================",
-  });
-  await notifyAboutNotStudy();
-  await webhook.send({
-    text: "====================================================================================",
-  });
-  await notifyAboutGrooming();
+const notifyAboutTasks = async (localMode = false) => {
+  await notifyAboutStudy(localMode);
+  await notifyAboutNotStudy(localMode);
+  await notifyAboutGrooming(localMode);
 };
 
-CREDENTIALS && SLACK_HOOK_URL && t();
+if (!CREDENTIALS || !SLACK_HOOK_URL) {
+  console.error("PROVIDE CREDENTIALS TO START");
+} else {
+  notifyAboutTasks();
+}
